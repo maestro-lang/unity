cbcec7bdbc7a799264dadd93fba55373f978d802
Matheus Lessa <matheus@fastmail.com>
2020-02-26 23:21:39 -0300

    organizando as coisas

---
+++ Assets/Maestro/Scripts/Runtime/MaestroEngine.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using Maestro;
+using Maestro.StdLib;
+using Maestro.Debug;
+using System.Text;
+
+[CreateAssetMenu(menuName = "Maestro/Maestro Engine")]
+public sealed class MaestroEngine : ScriptableObject
+{
+	public Mode mode = Mode.Debug;
+	public bool registerStdLib = true;
+
+	private Engine engine;
+	private Debugger debugger;
+	private StringBuilder cachedStringBuilder = new StringBuilder();
+
+	public bool TryCompile(string sourceName, string sourceContent, out Executable executable)
+	{
+		var source = new Source(sourceName, sourceContent);
+		var compileResult = engine.CompileSource(source, mode);
+		if (compileResult.TryGetExecutable(out executable))
+		{
+			return true;
+		}
+		else
+		{
+			cachedStringBuilder.Clear();
+			compileResult.FormatErrors(cachedStringBuilder);
+			Debug.LogError(cachedStringBuilder);
+
+			return false;
+		}
+	}
+
+	public bool TryExecute(Executable executable)
+	{
+		using (var scope = engine.ExecuteScope())
+		{
+			var executeResult = scope.Execute(executable);
+			if (executeResult.error.isSome)
+			{
+				cachedStringBuilder.Clear();
+				executeResult.FormatError(cachedStringBuilder);
+				executeResult.FormatCallStackTrace(cachedStringBuilder);
+				Debug.LogError(cachedStringBuilder);
+
+				return false;
+			}
+
+			return true;
+		}
+	}
+
+	private void OnEnable()
+	{
+		engine = new Engine();
+
+		if (registerStdLib)
+		{
+			engine.RegisterStandardCommands(Debug.Log);
+			engine.RegisterOperationCommands();
+			engine.RegisterTypeCommands();
+		}
+
+		if (mode == Mode.Debug)
+		{
+			debugger = new Debugger();
+			debugger.Start(47474);
+			engine.SetDebugger(debugger);
+		}
+	}
+
+	private void OnDisable()
+	{
+		if (debugger != null)
+			debugger.Stop();
+	}
+}
---
+++ Assets/Maestro/Scripts/Runtime/MaestroRunner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public sealed class MaestroRunner : MonoBehaviour
+{
+	public MaestroEngine engine;
+	[Multiline]
+	public string source;
+
+	private void Start()
+	{
+		engine.TryCompile("source", source, out var executable);
+		engine.TryExecute(executable);
+	}
+}
