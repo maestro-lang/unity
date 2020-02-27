cd3fb700a5d409a04213b388dfacb30f39a0178a
Matheus Lessa <matheus@fastmail.com>
2020-02-27 00:04:11 -0300

    mais umas coisas

---
+++ Assets/Maestro/Scripts/Editor/MaestroScriptCreator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using System.IO;
+
+public static class MaestroScriptCreator
+{
+	[MenuItem("Assets/Create/Maestro/Maestro Script", false, 10)]
+	public static void CreateScript()
+	{
+		var path = "Assets/";
+		if (Selection.activeObject != null)
+		{
+			path = AssetDatabase.GetAssetPath(Selection.activeObject);
+			if (!AssetDatabase.IsValidFolder(path))
+				path = Path.GetDirectoryName(path);
+		}
+
+		path = Path.Combine(path, "MaestroScript.maestro");
+		ProjectWindowUtil.CreateAssetWithContent(path, "");
+	}
+}
